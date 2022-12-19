998ee040996f121ad1f6171181e7ee04fa61ff4e
TerenceGrover <terencegrover@gmail.com>
2022-12-19 10:05:41 +0100

    updated to match Auth0 basis from serv branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hey, Welcome to MusiQ </h1>
+      <LoginButton />
+      <UserProfile />
+      <LogoutButton />
     </div>
   );
 }
