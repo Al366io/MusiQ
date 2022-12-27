61e9a4af890009b47fed788e8d99d2ebd6bbd79c
TerenceGrover <terencegrover@gmail.com>
2022-12-28 01:13:57 +0400

    Finally found a design direction

--- client/src/pages/Adding.jsx
+++ client/src/pages/Adding.jsx
@@ -3,21 +3,30 @@ import './styles/Adding.css'
 import Track from '../components/Adding/Track'
 import NextTrack from '../components/Adding/NextTrack'
 import AddButton from "../components/Adding/AddButton";
+import Separator from "../components/Adding/Separator";
+import { useContext } from "react";
+import { BGContext } from "../App";
 
 const Adding = () => {
+
+  const {BGcolor} = useContext(BGContext);
 
   let {id} = useParams()
 
   return(
     <div id="dash-back">
-      <div className="adding-inner-container">
+      <div className="adding-inner-container"  style={{backgroundColor : BGcolor}}>
         <div className="dashboard-container">
-          <h1>Room {id}</h1>
+          <h3 id="adding-dash"># {id} #</h3>
           <div className="next-track-container">
             <NextTrack/>
           </div>
           <div className="track-list-container">
-            <Track></Track>
+            <Track />
+            <Separator />
+            <Track />
+            <Separator />
+            <Track />
           </div>
         </div>
         <AddButton/>
