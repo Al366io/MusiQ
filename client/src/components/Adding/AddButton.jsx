101306e530630d1ac879a56953a1c7d1ec27d111
Al366io <alessio.nannipieri0@gmail.com>
2022-12-28 16:35:35 +0100

    gitgnore and minor

--- client/src/components/Adding/AddButton.jsx
+++ client/src/components/Adding/AddButton.jsx
@@ -1,6 +1,9 @@
 import '../styles/addbutton.css'
 
 const AddButton = () => {
+  // when clicking the + button, we should open a modal panel that let the adder search for a song 
+  // and then insert it. when doing so, send a request to backend with the party id 
+  //  
   return (
     <button onClick={(() => alert('Spotify Handler'))} id="adding-song-button">
       +
